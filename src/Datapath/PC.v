ec93bd22c7847e72aa1f54e1f0f8f217a46da3f7
HoshinoTouko <oranly@hotmail.com>
2017-11-20 01:41:13 +0800

    No commit info.

--- src/Datapath/PC.v
+++ src/Datapath/PC.v
@@ -1,1 +1,15 @@
+module pc(
+  input wire clk,
+  input	wire reset,
+  input	wire[31:0]	data,
+  output	reg[31:0]	dout
+);
 
+  always @(posedge clk)
+	begin
+		if(reset)
+			dout[31:0] <= {32'h0000_3000};
+		else
+			dout <= data;
+	end
+endmodule
